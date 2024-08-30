a42b3f6983cd46380dedacff95c4f2ba0425c8c7
himanshuverma13 <himanshu.verma@skillintelligence.tech>
2024-08-30 16:21:21 +0530

    getfromtestingtonewbranch

--- src/controller/user/transactionController.js
+++ src/controller/user/transactionController.js
@@ -1,10 +1,12 @@
 import Transaction from "../../model/user/transactionModel.js"
+import { sendTransactionWhatsapp } from "../../repository/user/trnsctToWhatsapp.js";
 import { deleteAfterTransaction } from "./addToCartController.js";
 
 export const createTransaction = async (req, res) => {
   try {
     const user = await Transaction.create(req.body);
     res.status(201).send("Transaction submitted successfully");
+    const result = sendTransactionWhatsapp(req?.body)
     deleteAfterTransaction(req.body.uid)
   } catch (error) {
     res.status(400).send(error.message);
